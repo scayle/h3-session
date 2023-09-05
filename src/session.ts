a8b3363b65f8cc8503bdfc01b7369e24d3f3e174
Connor Pearson <connor.pearson@aboutyou.com>
2023-09-05 10:36:12 +0200

    Rename session properties

--- src/session.ts
+++ src/session.ts
@@ -10,10 +10,10 @@ export interface SessionMethods {
 export type SessionT<SessionDataT> = SessionDataT & SessionMethods
 
 export class Session<SessionDataT> implements SessionMethods {
-  _id: string
-  _store: SessionStore<SessionDataT>
-  _data: SessionDataT
-  _generate: () => Promise<{ id: string; data: SessionDataT }>
+  #id: string
+  #store: SessionStore<SessionDataT>
+  data: SessionDataT
+  #generate: () => Promise<{ id: string; data: SessionDataT }>
   cookie: SessionCookie
 
   constructor(
@@ -23,39 +23,39 @@ export class Session<SessionDataT> implements SessionMethods {
     generate: () => Promise<{ id: string; data: SessionDataT }>,
     cookie: SessionCookie,
   ) {
-    this._data = data
-    this._id = id
-    this._store = store
-    this._generate = generate
+    this.data = data
+    this.#id = id
+    this.#store = store
+    this.#generate = generate
     this.cookie = cookie
   }
 
   get id() {
-    return this._id
+    return this.#id
   }
 
   async save() {
-    await this._store.set(this._id, { ...this._data, cookie: this.cookie })
+    await this.#store.set(this.#id, { ...this.data, cookie: this.cookie })
   }
 
   async reload() {
-    this._data =
-      (await this._store.get(this._id)) ?? (await this._generate()).data
+    this.data =
+      (await this.#store.get(this.#id)) ?? (await this.#generate()).data
   }
 
   async destroy() {
     // Delete the cookie by setting maxAge to 0
     this.cookie.maxAge = 0
 
-    await this._store.destroy(this._id)
+    await this.#store.destroy(this.#id)
   }
 
   async regenerate() {
-    await this._store.destroy(this._id)
-    const { data, id } = await this._generate()
+    await this.#store.destroy(this.#id)
+    const { data, id } = await this.#generate()
 
-    this._data = data
-    this._id = id
+    this.data = data
+    this.#id = id
 
     await Promise.all([this.cookie.setSessionId(id), this.save()])
   }
